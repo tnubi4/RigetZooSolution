a8794da227141a4bc03cdb6ed96bc70cea53ce3a
338062 <338062@BL-213-M23>
2025-01-20 10:25:05 +0000

    Add project files.

---
+++ RigetZooSolution/Data/Migrations/20250114133732_InitialCreate.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+//Database Migration
+
+namespace RigetZooSolution.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
